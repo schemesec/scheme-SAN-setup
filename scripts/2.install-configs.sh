dd386c621971c39bffd847566163d99806c32d7d
scheme <scheme@scheme.home.schemesec.com>
2025-10-21 18:55:02 -0400

    update patch-nvmf-autoconnect.service

--- scripts/2.install-configs.sh
+++ scripts/2.install-configs.sh
@@ -3,7 +3,8 @@
 apt-get update
 apt-get dist-upgrade -y
 
-apt install -y python3 python3-cffi python3-pyparsing git python-is-python3  python3-pip python3-setuptools
+apt install -y python3 python3-cffi python3-pyparsing git python-is-python3  python3-pip python3-setuptools libibverbs-dev
+
 
 cd /root/schemelin/
 #git clone git://git.infradead.org/users/hch/nvmetcli.git
@@ -13,6 +14,9 @@ python setup.py install
 
 cp /root/schemelin/nvmet.conf /etc/modules-load.d/nvmet.conf 
 
+cp /root/schemelin/mlx4.conf /etc/modprobe.d/mlx4.conf 
+
+
 grep -Ev '^\s*(#|$)' /etc/modules-load.d/nvmet.conf | xargs -r -n1 modprobe
 
 
@@ -20,4 +24,4 @@ cp "/root/schemelin/$(uname -n)_interfaces" /etc/network/interfaces
 
 ifreload -a
 
-update-initramfs -u
+update-initramfs -u -k all
